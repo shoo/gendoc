c438f4cbf8cb5bdb32e0c0b6e4062dbe494917e4
SHOO <zan77137@nifty.com>
2019-08-25 03:33:53 +0900

    Using ${DMD} for workaround

--- .travis.sh
+++ .travis.sh
@@ -9,7 +9,7 @@ function if_error_exit() {
 TEST_TARGET_ARCH=${TEST_TARGET_ARCH:-x86_64}
 COVERAGE_DIR=${COVERAGE_DIR:-.cov}
 COVERAGE_MERGE=${COVERAGE_MERGE:-true}
-DC=${DC:-dmd}
+DMD=${DMD:-dmd}
 
-dub run -a=${TEST_TARGET_ARCH} -b=unittest-cov -c=default --compiler=${DC} -- -a=${TEST_TARGET_ARCH}
-dub run :candydoc -a=${TEST_TARGET_ARCH} -b=unittest-cov -c=default --compiler=${DC} -- -a=${TEST_TARGET_ARCH} --gendocConfig=candydoc --gendocTarget=docs_candydoc
+dub run -a=${TEST_TARGET_ARCH} -b=unittest-cov -c=default --compiler=${DMD} -- -a=${TEST_TARGET_ARCH}
+dub run :candydoc -a=${TEST_TARGET_ARCH} -b=unittest-cov -c=default --compiler=${DMD} -- -a=${TEST_TARGET_ARCH} --gendocConfig=candydoc --gendocTarget=docs_candydoc
