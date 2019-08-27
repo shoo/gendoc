ff0c426646e97adb2a3af20387026f325e59f5d5
SHOO <zan77137@nifty.com>
2019-08-27 23:21:43 +0900

    Add integration test

--- .travis.sh
+++ .travis.sh
@@ -3,9 +3,17 @@
 set -eux -o pipefail
 
 TEST_TARGET_ARCH=${TEST_TARGET_ARCH:-x86_64}
-echo "--coverage_dir=${COVERAGE_DIR:-.cov}">.coverageopt
-echo "--coverage_merge=${COVERAGE_MERGE:-true}">>.coverageopt
+TESTTYPE=${TESTTYPE:-unittest}
 DMD=${DMD:-dmd}
 
-dub run -a=${TEST_TARGET_ARCH} -b=unittest-cov -c=default --compiler=${DMD} -- -a=${TEST_TARGET_ARCH}
-dub run :candydoc -a=${TEST_TARGET_ARCH} -b=unittest-cov -c=default --compiler=${DMD} -- -a=${TEST_TARGET_ARCH} --gendocConfig=candydoc --gendocTarget=docs/candydoc
+echo "--coverage_dir=${COVERAGE_DIR:-.cov}">.coverageopt
+echo "--coverage_merge=${COVERAGE_MERGE:-true}">>.coverageopt
+
+if [ "$TESTTYPE" == "unittest" ] ; then
+	dub run -a=${TEST_TARGET_ARCH} -b=unittest-cov -c=default --compiler=${DMD} -- -a=${TEST_TARGET_ARCH}
+	dub run :candydoc -a=${TEST_TARGET_ARCH} -b=unittest-cov -c=default --compiler=${DMD} -- -a=${TEST_TARGET_ARCH} --gendocConfig=candydoc --gendocTarget=docs/candydoc
+elif [ "$TESTTYPE" == "integration" ]; then
+	dub build -a=${TEST_TARGET_ARCH} -b=unittest-cov -c=default --compiler=${DMD}
+	build/gendoc -a=${TEST_TARGET_ARCH} --root=testcases/case001
+	# build/gendoc -a=${TEST_TARGET_ARCH} --root=testcases/case002
+fi
